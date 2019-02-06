92d277cd043d45fbdba0ebc67c287676978b4957
Victor Fajardo <fajardo.de.leon@gmail.com>
2019-02-06 13:36:36 -0500

    Base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { Section2Component } from './section2/section2.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SideNavComponent,
+    Section2Component,
+    FooterComponent
   ],
   imports: [
     BrowserModule
