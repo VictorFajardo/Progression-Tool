68f63b863ca12557d690995c14397c4524345d19
Victor Fajardo <fajardo.de.leon@gmail.com>
2019-03-15 16:57:38 -0400

    Friday

--- src/app/section6/section6.component.ts
+++ src/app/section6/section6.component.ts
@@ -7,41 +7,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Section6Component implements OnInit {
 
+  rating = false;
+
   constructor() { }
 
   ngOnInit() {
+    const self = this;
     // tslint:disable-next-line:only-arrow-functions
-    $('.star').hover(function() {
-      // console.log($(this));
-      // tslint:disable-next-line:radix
-      const id = parseInt($(this).attr('id').substr(-1));
-      let value: string;
-      switch (id) {
-        case 1:
-          value = 'Very Unhelpful';
-          break;
-        case 2:
-          value = 'Unhelpful';
-          break;
-        case 3:
-          value = 'Fairly Helpful';
-          break;
-        case 4:
-          value = 'Helpful';
-          break;
-        case 5:
-          value = 'Very Helpful';
-          break;
-        default:
-          break;
+    $('.star').hover(function () {
+      if (!self.rating) {
+        // tslint:disable-next-line:radix
+        const id = parseInt($(this).attr('id').substr(-1));
+        let value: string;
+        switch (id) {
+          case 1:
+            value = 'Very Helpful';
+            break;
+          case 2:
+            value = 'Helpful';
+            break;
+          case 3:
+            value = 'Fairly Helpful';
+            break;
+          case 4:
+            value = 'Unhelpful';
+            break;
+          case 5:
+            value = 'Very Unhelpful';
+            break;
+          default:
+            break;
+        }
+        $('#rating-text').html(value);
+        $('#rating-text').addClass('on');
       }
-      $('#rating-text').html(value);
-      $('#rating-text').addClass('on');
     },
-    // tslint:disable-next-line:only-arrow-functions
-    function() {
-      $('#rating-text').removeClass('on');
+      // tslint:disable-next-line:only-arrow-functions
+      function() {
+        if (!self.rating) {
+          $('#rating-text').removeClass('on');
+        }
+        // tslint:disable-next-line:only-arrow-functions
+      }).on('click', function(e) {
+        e.preventDefault();
+        if (!self.rating) {
+          self.rating = true;
+          $('#rating-title').html('Thank you for rating!');
+          // tslint:disable-next-line:radix
+          const id = parseInt($(this).attr('id').substr(-1));
+          console.log(id);
+          for (let i = 1; i <= 5; i++) {
+            $('#star-' + i).removeClass('star').addClass('off');
+            if (i < id) {
+              $('#star-' + i + ' img').attr('src', 'assets/icons/rate-star-grey.svg');
+            } else {
+              $('#star-' + i + ' img').attr('src', 'assets/icons/rate-star-filled.svg');
+            }
+          }
+        }
     });
   }
-
 }
